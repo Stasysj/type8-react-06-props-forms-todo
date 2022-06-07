5f631545a57e586a381d7a97bfe2476dad104fe6
Marius <marius@example.com>
2022-06-07 11:40:17 +0300

    setIsDone((prevIsDone) => !prevIsDone)

--- src/TodoApp/SingleTodo/SingleTodo.jsx
+++ src/TodoApp/SingleTodo/SingleTodo.jsx
@@ -12,10 +12,15 @@ function SingleTodo({ title }) {
 
   // kai paspaudziu ant rutulioko ikoneles iskonsolinu "paspaudei"
   // kai paspaudziu ant <i className={`fa fa-home`} aria-hidden='true'></i> iskonsolinu "paspaudei"
-  // kai paspaudziu ant rutulioko ikoneles pakeiciu isDone statusa i priesinga
 
   function clickHandler() {
+    // kai paspaudziu ant rutulioko ikoneles pakeiciu isDone statusa i priesinga
     console.log('paspaudei');
+    // pakeisti statusa i true
+    // setIsDone(true); // kai state nepriklauso nuo pries tai buvusios
+    // pakeisti statusa i priesinga dabartiniam
+    // setIsDone(!isDone); // kai state nepriklauso nuo pries tai buvusios
+    setIsDone((prevIsDone) => !prevIsDone); // kai state priklauso nuo pries tai buvusios
   }
 
   return (
@@ -24,7 +29,6 @@ function SingleTodo({ title }) {
         onClick={clickHandler}
         icon={isDone === true ? 'fa-check-circle' : 'fa-circle-thin'}
       />
-      <i onClick={clickHandler} className={`fa fa-home`} aria-hidden='true'></i>
       <span className='text'>{title}</span>
       <Icon icon='fa-pencil' />
       <Icon icon='fa-trash' />
