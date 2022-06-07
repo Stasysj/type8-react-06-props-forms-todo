833596e041d6c9da9b2adc2963d3d785e2ef2f55
Marius <marius@example.com>
2022-06-07 10:26:51 +0300

    Icon SingleTodo

--- src/TodoApp/TodoList/TodoList.jsx
+++ src/TodoApp/TodoList/TodoList.jsx
@@ -1,13 +1,16 @@
+import SingleTodo from '../SingleTodo/SingleTodo';
+
+const initTodos = [
+  { id: 1, title: 'Do Sports', isDone: true },
+  { id: 2, title: 'Go Hiking', isDone: false },
+];
+
 function TodoList() {
   return (
     <section className='content'>
       <ul id='list' className='list'>
-        <li className='item '>
-          <i className='fa fa-circle-thin make-done' aria-hidden='true'></i>
-          <span className='text'> new todo</span>
-          <i className='fa fa-pencil edit-icon' aria-hidden='true'></i>
-          <i className='fa fa-trash delete-icon' aria-hidden='true'></i>
-        </li>
+        <SingleTodo {...initTodos[0]} />
+
         <li className='item line-through'>
           <i className='fa fa-check-circle make-done' aria-hidden='true'></i>
           <span className='text'> done todo</span>
