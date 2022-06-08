6456958c524ade2a944f7d8c68dbf4c410cbae61
Marius <marius@example.com>
2022-06-08 09:36:14 +0300

    handleDelete

--- src/TodoApp/TodoList/TodoList.jsx
+++ src/TodoApp/TodoList/TodoList.jsx
@@ -24,6 +24,11 @@ function TodoList() {
   // atnaujinti state su kopija state kurioje nera to el kuri trynem
   function handleDelete(deleteId) {
     console.log('handleDelete was initiated by todo with id', deleteId);
+
+    // isfiltruoti mainTodoArray ir gauti masyva kuriame nera objeto kurio id === deleteId
+    const filtered = mainTodoArray.filter((tObj) => tObj.id !== deleteId);
+    console.log('filtered ===', filtered);
+    setMainTodoArray(filtered);
   }
 
   return (
