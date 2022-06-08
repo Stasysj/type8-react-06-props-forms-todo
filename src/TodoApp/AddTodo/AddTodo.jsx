f3a022ff7e2977736299e53208352903ed9d6e9a
Marius <marius@example.com>
2022-06-08 11:47:57 +0300

    useRef current focus

--- src/TodoApp/AddTodo/AddTodo.jsx
+++ src/TodoApp/AddTodo/AddTodo.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Icon from '../UI/Icon';
 
 function AddTodo(props) {
+  const inputEl = useRef();
   // AddTodo sukurti state
   const [newTodoInput, setNewTodoInput] = useState('');
   // AddTodo susieti state su input
   // kai AddTodo kvieciam handleAddTodo argumentu paduojam input reiksme (isState)
+
+  function handleNewTodo() {
+    props.onAddTodo(newTodoInput);
+    setNewTodoInput('');
+    // sufokusuoti kursoriu i input
+    inputEl.current.focus();
+  }
+
   return (
     <div className='add-item'>
-      <Icon onClick={props.onAddTodo} icon='fa-plus-circle' />
+      <Icon onClick={handleNewTodo} icon='fa-plus-circle' />
       <input
+        ref={inputEl}
         type='text'
         placeholder='Add todo'
         onChange={(event) => setNewTodoInput(event.target.value)}
